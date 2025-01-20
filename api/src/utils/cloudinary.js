4ab3d97b299b6f78136b58507b0c759d7e6c3f8e
Beni Nurfauzi <beni@fungsitama.com>
2025-01-20 07:06:23 +0700

    fix error cloudinary

--- api/src/utils/cloudinary.js
+++ api/src/utils/cloudinary.js
@@ -1,21 +1,19 @@
-import { v2 } from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import 'dotenv/config'
 import moment from 'moment'
 
-const cloud = v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 })
 
 export const cloudinaryUpload = async (file) => {
-
   let data = { imageId: undefined, imageUrl: undefined }
 
   if (file) {
-
     try {
-      data = await cloud.uploader.upload(
+      data = await cloudinary.uploader.upload(
         file.tempFilePath,
         {
           public_id: new Date().getTime(),
@@ -32,33 +30,35 @@ export const cloudinaryUpload = async (file) => {
 }
 
 export const cloudinaryDelete = async (id) => {
-  await cloud.uploader.destroy(id)
+  if (!id) return
+  try {
+    await cloudinary.uploader.destroy(id)
+  } catch (error) {
+    console.log(`[ ${moment().format('DD/MM/YYYY HH:mm:ss')} ] ${error}`)
+    throw error
+  }
 }
 
 export const cloudinaryReUpload = async (id, file) => {
-
   let data = { imageId: undefined, imageUrl: undefined }
+  
   if (file) {
-
     try {
-
       if (id) {
         await cloudinaryDelete(id)
       }
 
-      data = await cloud.uploader.upload(
+      data = await cloudinary.uploader.upload(
         file.tempFilePath,
         {
           public_id: new Date().getTime(),
           folder: 'ekasir'
         }
       )
-
     } catch (error) {
       console.log(`[ ${moment().format('DD/MM/YYYY HH:mm:ss')} ] ${error}`)
       throw error
     }
-
   }
 
   return { imageId: data.public_id, imageUrl: data.secure_url }
